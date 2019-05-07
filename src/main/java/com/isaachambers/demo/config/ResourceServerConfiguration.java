83a5c28146964d1aa3e99e29ce17dcb60eb3d24e
isaachambers <isaaceduardk@gmail.com>
2019-05-08 02:40:23 +0300

    Adding OAuth Configuration

--- src/main/java/com/isaachambers/demo/config/ResourceServerConfiguration.java
+++ src/main/java/com/isaachambers/demo/config/ResourceServerConfiguration.java
@@ -1,6 +1,7 @@
 package com.isaachambers.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -9,7 +10,8 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/customer/**").hasRole("ADMIN").anyRequest()
+				.authenticated();
 	}
 
 }
